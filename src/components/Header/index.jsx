de32bb0ff12c0150db67f7a14573047b86fa738e
Jackson <jackson1712@hotmail.com>
2023-01-15 17:07:35 -0300

    added component of header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from "./styles";
+
+export function Header() {
+    return (
+        <Container>
+            <Profile>
+                <img 
+                src="https://github.com/jackson1712.png" 
+                alt="Imagem de perfil do usuário"
+                />
+
+                <div>
+                    <span>Bem-Vindo</span>
+                    <strong>Jackson Moura</strong>
+                </div>
+            </Profile>
+
+        <Logout>
+            <RiShutDownLine/>
+        </Logout>
+
+        </Container>
+    )
+}
