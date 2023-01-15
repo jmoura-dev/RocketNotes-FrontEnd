31be8e278fbcc55e58f2f58f47482f455700e220
Jackson <jackson1712@hotmail.com>
2023-01-15 17:09:12 -0300

    added components of style from content

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,44 @@
-import { Container } from './styles'
+import { Container, Links, Content } from './styles'
 import { Button } from '../../components/button'
+import { Header } from '../../components/header'
+import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details() {
 
   return (
     <Container>
-      <h1>Hello, World!</h1>
-    <Button title="Login" loading />
-    <Button title="Sign up" />
+    <Header/>
+
+    <main>
+    <Content>
+
+    <ButtonText title="Excluir nota"/>
+    
+    <h1>Intrução ao React</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
+      vel nihil! Maiores tenetur sint ipsa veritatis placeat dolorem ut exercitationem,
+      consequatur ex architecto. Nobis, dignissimos est iure tempore consectetur excepturi?</p>
+
+    
+
+    <Section title="Links úteis">
+      <Links>
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 2</a></li>
+      </Links>
+    </Section>
+
+    <Section title="Marcadores">
+    <Tag title="Node" />
+    <Tag title="React" />
+    </Section>
+
     <Button title="Return" />
-      </Container>
+
+      </Content>
+    </main>
+    </Container>
   )
 };
