c8b0f769049a983922fff311b4f995b4f143b30d
Jackson <jackson1712@hotmail.com>
2023-01-16 16:00:29 -0300

    added and styled page of login

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,11 @@
+import { FiPlus, FiSearch } from 'react-icons/fi'
+
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from "../../components/Header";
 import { ButtonText } from "../../components/ButtonText";
+import { Input } from '../../components/Input';
+import { Section } from '../../components/Section';
+import { Note } from '../../components/Note';
 
 
 export function Home() {
@@ -19,11 +24,25 @@ export function Home() {
                 <li><ButtonText title="Nodejs" /></li>
             </Menu>
 
-            <Search></Search>
+            <Search>
+                <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
+            </Search>
+            <Content>
+            <Section title="Minhas notas">
+                <Note data={{
+                    title:"React",
+                    tags: [
+                        {id: '1', name: 'React'},
+                        {id: '2', name: 'Node'},
+                    ]
+            }}/>
+            </Section>
+            </Content>
 
-            <Content></Content>
-
-            <NewNote></NewNote>
+            <NewNote>
+                <FiPlus/>
+                Criar nota
+            </NewNote>
         </Container>
     );
 }
